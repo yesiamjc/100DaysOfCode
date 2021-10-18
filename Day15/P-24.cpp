3a5045230d07a6c405c13624ca6a6b91558c7143
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2021-10-18 18:13:21 +0530

    Day 15 Code Upload

---
+++ Day15/P-24.cpp
@@ -0,0 +1,17 @@
+//Rectangle pattern :
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
