f1fb6cbbd8f45f58324fabf8efa591fb19e9467a
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2021-11-08 20:43:38 +0530

    Day 36 Code Upload

---
+++ Day36/P-66.cpp
@@ -0,0 +1,36 @@
+// Given an array arr[] of size N, enqueue the elements of the array into a queue and then dequeue them.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+queue<int>_push(int arr[],int n);
+void _pop(queue<int>s);
+queue<int>_push(int arr[],int n)
+{
+ queue<int>q;
+ for(int i=0;i<n;i++)
+ {
+  q.push(arr[i]);
+ }
+ return q;
+}
+void _pop(queue<int>s)
+{
+  while(!s.empty())
+  {
+    cout<<s.front()<<" ";
+    s.pop();
+  }
+} 
+int main()
+ {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	 cin>>arr[i];
+	queue<int>mys =_push(arr,n);
+	_pop(mys);
+	cout<<endl;
+	return 0;
+ }
