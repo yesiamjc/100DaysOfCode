3bee1b983e9cd39b8880a856f2b44132a6b43dab
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2021-12-17 22:30:33 +0530

    Day 75 Code Upload

---
+++ Day75/P-108.cpp
@@ -0,0 +1,60 @@
+
+// Given an array of size N. The task is to sort the array elements by completing functions heapify() and 
+// buildHeap() which are used to implement Heap Sort.
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void heapify(int arr[], int n, int i)
+    {
+        int l = 2 * i + 1, r = 2 * i + 2, largest = i;
+        if (l < n and arr[l] > arr[i])
+            largest = l;
+        else
+            largest = i;
+        if (r < n and arr[r] > arr[largest])
+            largest = r;
+        if (i != largest)
+        {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+public:
+    void buildHeap(int arr[], int n)
+    {
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+    }
+
+public:
+    void heapSort(int arr[], int n)
+    {
+        buildHeap(arr, n);
+        for (int i = n - 1; i > 0; i--)
+        {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+};
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main()
+{
+    int arr[1000000], n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
